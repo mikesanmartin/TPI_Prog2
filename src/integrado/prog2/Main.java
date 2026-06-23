e63637545b4eaad8f986a6276c594d020677a6bf
Michel San Martin <mikeemmanuel91@gmail.com>
2026-06-23 20:14:08 -0300

    added services and minor fixes

--- src/integrado/prog2/Main.java
+++ src/integrado/prog2/Main.java
@@ -1,5 +1,9 @@
+import services.CategoriaService;
+
 public class Main {
     public static void main(String[] args){
-        
+        CategoriaService categoriaService = new CategoriaService();
+
+        System.out.println("Look: " + categoriaService.getCategoriasActivas());
     }
 }
--- src/integrado/prog2/enums/Estado.java
+++ src/integrado/prog2/enums/Estado.java
@@ -1,5 +1,8 @@
 package enums;
 
 public enum Estado {
-
+    PENDIENTE,
+    CONFIRMADO,
+    TERMINADO,
+    CANCELADO
 }
