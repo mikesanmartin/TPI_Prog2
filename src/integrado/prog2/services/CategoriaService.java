e4133f30276f5c4bae9f84baf6cc18baec7eb7bc
Michel San Martin <mikeemmanuel91@gmail.com>
2026-06-23 22:31:32 -0300

    added Categoria Services

--- src/integrado/prog2/services/CategoriaService.java
+++ src/integrado/prog2/services/CategoriaService.java
@@ -2,19 +2,22 @@ package services;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import entities.Categoria;
 
 public class CategoriaService {
     private final List<Categoria> categorias;
-    private int ultimoId;
+    private Long ultimoId;
+
+    Scanner scanner = new Scanner(System.in);
 
     public CategoriaService() {
         this.categorias = new ArrayList<>();
-        this.ultimoId = 0;
+        this.ultimoId = 0L;
     }
 
-    public List<Categoria> getCategoriasActivas(){
+    public List<Categoria> listarCategorias(){
         List<Categoria> categoriasActivas = new ArrayList<>();
         for (Categoria categoria: categorias){
             if(!categoria.isEliminado()){
@@ -22,5 +25,39 @@ public class CategoriaService {
             }
         }
         return categoriasActivas;
+    }
+
+    public boolean crearCategoria(String nombre, String descripcion){
+        for(Categoria categoria : categorias){
+            if(!categoria.isEliminado() && categoria.getNombre().equalsIgnoreCase(nombre)){
+                return false;
+            }
+        }
+
+        ultimoId++;
+        Categoria nuevaCategoria = new Categoria(ultimoId,nombre,descripcion);
+        categorias.add(nuevaCategoria);
+        return true;
+    }
+
+    public boolean editarCategoria(Long id, String nuevoNombre, String nuevaDescripcion){
+        for(Categoria categoria: categorias){
+            if(categoria.getId().equals(id) && !categoria.isEliminado()){
+                categoria.setNombre(nuevoNombre);
+                categoria.setDescripcion(nuevaDescripcion);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean eliminarCategoria(Long id){
+        for(Categoria categoria: categorias){
+            if(categoria.getId().equals(id) && !categoria.isEliminado()){
+                categoria.setEliminado(true);
+                return true;
+            }
+        }
+        return false;
     }
 }
